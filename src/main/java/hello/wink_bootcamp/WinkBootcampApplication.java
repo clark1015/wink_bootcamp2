0be67cfaf17f1b23825cbe3573e59980a92c6392
junhwan seok <seok626898@gmail.com>
2025-07-24 20:54:19 +0900

    initial commit

---
+++ src/main/java/hello/wink_bootcamp/WinkBootcampApplication.java
@@ -0,0 +1,13 @@
+package hello.wink_bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinkBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinkBootcampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/wink_bootcamp/WinkBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.wink_bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinkBootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
