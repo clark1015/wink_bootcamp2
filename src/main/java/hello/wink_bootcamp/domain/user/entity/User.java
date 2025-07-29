9c72ab08211813aa4ae42e76a3da1f8883951cd6
Jeongdaeun1 <daeun1q2w3e@gmail.com>
2025-07-29 15:10:07 +0900

    jwt인증

---
+++ src/main/java/hello/wink_bootcamp/domain/user/entity/User.java
@@ -0,0 +1,30 @@
+package hello.wink_bootcamp.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userid;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true, length = 30)
+    private String username;
+
+}
