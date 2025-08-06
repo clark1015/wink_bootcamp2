3587135ff20ba6542655b4b03dc220c07286ea04
clark1015 <seok626898@gmail.com>
2025-08-07 01:40:47 +0900

    [pro] 테스트용 페이지 경로에서 프론트 url로 변경

--- src/main/java/hello/wink_bootcamp/domain/oauth/handler/OAuth2SuccessHandler.java
+++ src/main/java/hello/wink_bootcamp/domain/oauth/handler/OAuth2SuccessHandler.java
@@ -93,7 +93,7 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
                                     HttpServletRequest request,
                                     HttpServletResponse response) throws IOException {
 
-        String redirectUri = "http://auth.junhwan.me/hello"
+        String redirectUri = "http://suntcamp-auth.junhwan.me/home"
                 + "?accessToken=" + accessToken
                 + "&refreshToken=" + refreshToken
                 + "&message=login_success";
