123591ca0801920312ffb10f10b488c9ebddb2ad
clark1015 <seok626898@gmail.com>
2025-08-06 18:23:50 +0900

    [fix] oauth2 filter와 일반 login url 분리

--- src/main/java/hello/wink_bootcamp/global/config/SecurityConfig.java
+++ src/main/java/hello/wink_bootcamp/global/config/SecurityConfig.java
@@ -39,22 +39,30 @@ public class SecurityConfig {
                 .authorizeHttpRequests(
                         auth->auth
                                 .requestMatchers(
-                                        "/api/auth/**",
+                                        "/api/auth/signin",      // 개별적으로 명시
+                                        "api/auth/email",
+                                        "api/auth/email/verify",
+                                        "/api/auth/register",
+                                        "/api/auth/refresh",
+                                        "/api/auth/logout",
+                                        "/api/email/**",         // 이메일 인증 관련
                                         "/v3/api-docs/**",
-                                        "/oauth2/**",
                                         "/hello",
                                         "/swagger-ui/**",
                                         "/swagger-ui.html"
-                                        ).permitAll()
+                                ).permitAll()
+                                .requestMatchers("/oauth2/**", "/login/oauth2/**").permitAll()  // OAuth2만 따로 허용
                                 .anyRequest().authenticated()
                 )
                 .oauth2Login(oauth2 -> oauth2
+                        .loginPage("/oauth2/authorization/kakao")  // 명시적 로그인 페이지 설정
+                        .authorizationEndpoint(authorization ->
+                                authorization.baseUri("/oauth2/authorization")  // OAuth2 시작점을 명확히 설정
+                        )
                         .userInfoEndpoint(userinfo -> userinfo
                                 .userService(customOAuth2UserService)
                         )
                         .successHandler(oAuth2SuccessHandler)
-//                        .failureHandler(failureHanlder)
-
                 )
 
                 .addFilterBefore(exceptionHandlerFilter(), UsernamePasswordAuthenticationFilter.class)
